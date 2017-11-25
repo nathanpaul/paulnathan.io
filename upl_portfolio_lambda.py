cc74aa71703cfc07ba45d95f5ba3b5f7d6307f0c
Paul Nathan <nathanp1@tcnj.edu>
2017-11-24 23:56:38 -0500

    modified lambda code

--- upl_portfolio_lambda.py
+++ upl_portfolio_lambda.py
@@ -1,19 +1,23 @@
 import boto3
 import StringIO
 from botocore.client import Config
-import mimetypes
+import zipfile
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-portfolio_bucket = s3.Bucket('portfolio.paulnathan.io')
-build_bucket = s3.Bucket('portfoliobuild.paulnathan.io')
+    portfolio_bucket = s3.Bucket('portfolio.paulnathan.io')
+    build_bucket = s3.Bucket('portfoliobuild.paulnathan.io')
 
-portfolio_zip = StringIO.StringIO()
+    portfolio_zip = StringIO.StringIO()
 
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm)
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm)
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    print "Hello world"
+    return "Welcome to Lambda!"
