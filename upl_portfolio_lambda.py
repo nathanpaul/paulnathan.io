3da8e5d15051ea19ac96d0e59d86eb1b272f5437
Paul Nathan <nathanp1@tcnj.edu>
2017-11-30 23:43:01 -0500

    Checks for proper mimetypes that aren't covered by Python 2.7 mimetypes + ignores ds_store

--- upl_portfolio_lambda.py
+++ upl_portfolio_lambda.py
@@ -2,8 +2,10 @@ import boto3
 import StringIO
 from botocore.client import Config
 import zipfile
+import mimetypes
 
 def lambda_handler(event, context):
+    mimetypes.init()
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:092746047212:deploy_portfolio_topic')
     location = {
@@ -29,8 +31,32 @@ def lambda_handler(event, context):
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
+                if nm.endswith('DS_Store'):
+                    continue
                 obj = myzip.open(nm)
-                portfolio_bucket.upload_fileobj(obj, nm)
+                if nm.endswith('eot'):
+                    portfolio_bucket.upload_fileobj(obj, nm,
+                        ExtraArgs={'ContentType': 'application/vnd.ms-fontobject'})
+                elif nm.endswith('woff2'):
+                    portfolio_bucket.upload_fileobj(obj, nm,
+                        ExtraArgs={'ContentType': 'font/woff2'})
+                elif nm.endswith('otf'):
+                    portfolio_bucket.upload_fileobj(obj, nm,
+                        ExtraArgs={'ContentType': 'font/otf'})
+                elif nm.endswith('woff'):
+                    portfolio_bucket.upload_fileobj(obj, nm,
+                        ExtraArgs={'ContentType': 'font/woff'})
+                elif nm.endswith('less'):
+                    portfolio_bucket.upload_fileobj(obj, nm,
+                        ExtraArgs={'ContentType': 'text/css'})
+                elif nm.endswith('ttf'):
+                    portfolio_bucket.upload_fileobj(obj, nm,
+                        ExtraArgs={'ContentType': 'application/octet-stream'})
+                else:
+                    print nm
+                    portfolio_bucket.upload_fileobj(obj, nm,
+                        ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0] })
+
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
         print "Hello world"
